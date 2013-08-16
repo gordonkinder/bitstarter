66ac24a26661fe22ed6b0302dbc0fec98ddab974
Gordon Kinder <gordon.kinder@gmail.com>
2013-08-16 14:32:19 +0000

    reinitialized web.js to original

--- web.js
+++ web.js
@@ -1,16 +1,18 @@
-#!usr/bin/env node
+#!/usr/bin/env node
+
 var express = require('express');
-var app = express();
+var fs - require('fs');
+var htmlfile = "index.html";
+
+var app = express.createServer (experess.logger());
 app.use(express.logger());
 
-var fs=require('fs');
-
 app.get('/', function(request, response) {
-//response.send('Hello World2 !');
-  response.send(fs.readFileSync('index.html').toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
