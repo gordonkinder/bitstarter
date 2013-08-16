3e4986b10c0a19537166e9333da63972e0e6cc25
Gordon Kinder <gordon.kinder@gmail.com>
2013-08-16 15:11:17 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var express = require('express');
-var fs - require('fs');
+var fs = require('fs');
 var htmlfile = "index.html";
 
 var app = express.createServer (experess.logger());
